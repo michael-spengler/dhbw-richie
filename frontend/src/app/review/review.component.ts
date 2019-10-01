e61b539d0e025ed41306b130f80457c268bd74a7
Timo Scheuermann <timo.scheuermann@gmx.de>
2019-10-01 23:35:57 +0200

    restyled paragraph, added /settings, fixed nicos button :)

--- frontend/src/app/review/review.component.ts
+++ frontend/src/app/review/review.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-review',
-  templateUrl: './review.component.html',
-  styleUrls: ['./review.component.css']
+  selector: "app-review",
+  templateUrl: "./review.component.html",
+  styleUrls: ["./review.component.css"]
 })
 export class ReviewComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public display = { display: "none" };
+  public togglePopUp() {
+    this.display = {
+      display: this.display.display == "none" ? "block" : "none"
+    };
   }
-
 }
