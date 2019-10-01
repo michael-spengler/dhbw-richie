b56a4b719bb5180746d981a27d43bde9427f5979
Timo Scheuermann <timo.scheuermann@gmx.de>
2019-10-01 16:26:33 +0200

    Added Routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+import { AddComponent } from "./add/add.component";
+import { AdminComponent } from "./admin/admin.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ReviewComponent } from "./review/review.component";
+import { SearchComponent } from "./search/search.component";
+import { TelegramComponent } from "./telegram/telegram.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
-import { AddComponent } from './add/add.component';
-import { ReviewComponent } from './review/review.component';
+const appRoutes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent,
+    data: { title: "Heroes List" }
+  },
+  { path: "add", component: AddComponent },
+  { path: "review", component: ReviewComponent },
+  { path: "search", component: SearchComponent },
+  { path: "telegram", component: TelegramComponent },
+  { path: "admin", component: AdminComponent },
+
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
     AddComponent,
-    ReviewComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    ReviewComponent,
+    TelegramComponent,
+    PageNotFoundComponent,
+    AdminComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/telegram/telegram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-telegram',
+  templateUrl: './telegram.component.html',
+  styleUrls: ['./telegram.component.css']
+})
+export class TelegramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
