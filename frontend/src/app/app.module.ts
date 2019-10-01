8022be0a88fd0a2f47349bc7ff8949a6e8f86d52
Timo Scheuermann <timo.scheuermann@gmx.de>
2019-10-01 14:55:03 +0200

    Added Basic Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { AddComponent } from './add/add.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    AddComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
