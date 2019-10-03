db34a8447992eceff14331de5b862bf39ab4dbff
Salocinf <nicolas.fuerhaupter@me.com>
2019-10-03 16:38:56 +0200

    Implemented for-loop for table

--- frontend/src/app/review/review.component.ts
+++ frontend/src/app/review/review.component.ts
@@ -6,14 +6,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./review.component.css"]
 })
 export class ReviewComponent implements OnInit {
-  constructor() {}
+
+  myItems: any[]; //just for testing purposes
+
+  constructor() {
+    this.myItems = [
+      { status: "Neu", question: "Was ist der Sinn des Lebens", answer: 42 },
+      { status: "Änderung", question: "Was ist der Sinn des Lebens", answer: 42 },
+      { status: "Feedback", question: "Was ist der Sinn des Lebens", answer: 42 },
+      { status: "Löschen", question: "Was ist der Sinn des Lebens", answer: 42 },
+    ]
+  }
 
   ngOnInit() {}
 
   public display = { display: "none" };
+
   public togglePopUp() {
     this.display = {
       display: this.display.display == "none" ? "block" : "none"
     };
+  }
+
+  // Q&A Inputfields
+  questionInput;
+  answerInput;
+  sourceInput;
+
+  // Dropdown
+  lectureInput;
+  lectureOutput;
+
+  onChange() {
+    // tslint:disable-next-line: max-line-length
+
+    window.alert(
+      "Question: " +
+        this.questionInput +
+        "\nAnswer: " +
+        this.answerInput +
+        "\nSource: " +
+        this.sourceInput
+    );
+    this.questionInput = "";
+    this.lectureInput = "";
+    this.sourceInput = "";
+    this.answerInput = "";
   }
 }
