1d069ef3c5b2f9a51b8a9878fdce9eb6a0580962
Aaron Schweig <aaronschweig@gmail.com>
2019-10-20 19:33:30 +0200

    Fixed theme

--- frontendAlpha/src/app/information/information.module.ts
+++ frontendAlpha/src/app/information/information.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ImpressumComponent } from './impressum/impressum.component';
@@ -26,6 +27,6 @@ const commonRoutes: Routes = [
 ];
 @NgModule({
   declarations: [HomeComponent, LegalComponent, AboutComponent, ImpressumComponent],
-  imports: [CommonModule, RouterModule.forChild(commonRoutes)]
+  imports: [CommonModule, RouterModule.forChild(commonRoutes), SharedModule]
 })
 export class InformationModule {}
